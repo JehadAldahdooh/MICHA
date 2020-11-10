88bd09a1a80ad9f7825aa0fe4dad63c64b4c8fa8
root <root@micha.novalocal>
2020-11-10 15:17:56 +0000

    second_v4

---
+++ js/home_page.js
@@ -0,0 +1,90 @@
+function clearText(field)
+{
+    if (field.defaultValue == field.value) field.value = '';
+    else if (field.value == '') field.value = field.defaultValue;
+}
+
+
+
+
+        <!-- jQuery code to show the working of this method -->
+        $(document).ready(function() { 
+		
+				$("#step2").click(function(event) {
+		  alert("Please upload the file first");
+		
+		});
+		$("#step3").click(function(event) {
+		  alert("Please upload the file first");
+		
+		});
+
+
+
+            $("#upload_id").click(function(event) { 
+			  event.preventDefault();
+$('#upload_id').click(false);
+
+                //$(this).fadeOut();
+console.log("sdsdsdsss");
+    $('html,body').animate({
+        scrollTop: $(".style_upload").offset().top},
+        'slow');
+
+
+	document.getElementById("style1").style.display = "none";
+	document.getElementById("style2").style.display = "none";
+	document.getElementById("style3").style.display = "none";
+	document.getElementById("style_upload").style.display = "block";
+
+                 				
+            }); 
+			
+	
+			
+    $('#submit').attr('disabled', 'disabled');
+    var a = 0;
+    //binds to onchange event of your input field
+    $('#file').bind('change', function() {
+        if ($('input:submit').attr('disabled', false)) {
+            $('input:submit').attr('disabled', true);
+        }
+        var ext = $('#file').val().split('.').pop().toLowerCase();
+        if ($.inArray(ext, ['xlsx']) == -1) {
+            $('#error1').slideDown("slow");
+            $('#error2').slideUp("slow");
+            $('#submit').attr('disabled', 'disabled');
+
+            a = 0;
+        } else {
+            var picsize = (this.files[0].size);
+            if (picsize > 40000000) {
+                $('#error2').slideDown("slow");
+                //this is to disable the button to prevent the user to upload any file
+                $('#submit').attr('disabled', 'disabled');
+
+                a = 0;
+            } else {
+                a = 1;
+                $('#submit').removeAttr('disabled');
+                $('#error2').slideUp("slow");
+            }
+            $('#error1').slideUp("slow");
+
+            if (a == 1) {
+                $('input:submit').attr('disabled', false);
+            }
+        }
+    });
+
+
+    $(".upload-image").click(function() {
+        document.getElementById("uploadmessage").style.display = "block";
+        document.getElementById("dropme").style.display = "none";
+        $('#uploadmessage').text("Please Wait until Upload process is complete");
+        document.getElementById("loadbar").style.display = "block";
+        $(".form-horizontal").submit();
+    });
+	
+			
+        }); 
--- theme_files/search_compound.php
+++ theme_files/search_compound.php
@@ -1,8 +1,14 @@
 <div id="templatemo_search">
-   <div id="search_box">
-      <form action="#" method="get">
-         <input type="text" value="Enter a keyword..." name="q" size="10" id="searchfield" title="searchfield" onfocus="clearText(this)" onblur="clearText(this)" />
-         <input type="submit" name="Search" value="" alt="Search" id="searchbutton" title="Search" />
-      </form>
-   </div>
-</div>
+
+	<div id="search_box">
+<!--	<script async src="https://cse.google.com/cse.js?cx=017046730853086508825:sckkkfpde90"></script>
+<div class="gcse-search"></div>-->
+
+		<form action="/compound" method="get">
+			<input required type="text" value="Enter compound name..." name="c_name" size="10" id="searchfield" title="searchfield" 
+			onfocus="clearText(this)" onblur="clearText(this)" />
+			<input type="submit" name="" value="" alt="Search" id="searchbutton" title="Search" />
+		</form>
+	</div>
+
+</div> <!-- end of search -->
